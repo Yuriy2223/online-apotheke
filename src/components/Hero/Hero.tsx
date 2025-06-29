abd6718e454643aac8d6bae09fdab1c2395146d0
Yuriy <>
2025-06-29 23:47:58 +0300

    create promo baners

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import { Container } from "@/shared/Container";
+
+export const Hero = () => {
+  return (
+    <section className="bg-green-light h-[400px] tablet:h-[700px] desctop:h-[800px] flex justify-center items-center">
+      <Container className="relative">
+        <Image
+          src="/images/hero.webp"
+          alt="Description"
+          width={750}
+          height={508}
+          sizes="(max-width: 375px) 330px, (max-width: 768px) 704px, 750px"
+          className="w-full max-w-[750px] h-auto  mx-auto"
+          priority //  важливо пріорететне завантаження!
+        />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+          <div className="pt-[20px] tablet:pt-[140px] tablet:pl-[32px] tablet:pr-[83px] bg-img h-[312px] w-[330px] max-tablet:w-[280px] tablet:h-[508px] tablet:w-auto lg:w-[750px] mx-auto">
+            <h1 className="w-[292px] tablet:w-[600px] font-semibold text-[50px] tablet:text-[74px] leading-[1] text-white-true mb-[20px] tablet:mb-[24px]">
+              Your medication delivered
+            </h1>
+            <p className="w-[150px] tablet:w-[200px] font-normal text-[12px] tablet:text-[16px] leading-[1.25] text-white-true ml-auto">
+              Say goodbye to all your healthcare worries with us
+            </p>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
---
+++ src/types/pharmacies.ts
@@ -0,0 +1,29 @@
+export interface Pharmacie {
+  _id: string;
+  name: string;
+  city: string;
+  phone: string;
+  address: string;
+  rating: number;
+  url?: string;
+  openTime: string;
+  closeTime: string;
+}
+
+// export interface Pharmacie {
+//   _id: string;
+//   name: string;
+//   city: string;
+//   phone: string;
+//   address: string;
+//   rating: string;
+//   status: "OPEN" | "CLOSE";
+// }
+export interface NearestPharmacie {
+  _id: string;
+  name: string;
+  address: string;
+  city: string;
+  phone: string;
+  rating: string;
+}
