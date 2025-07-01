de90a5899c56d0390305ea69eb33bf688d78520a
Yuriy <>
2025-07-01 14:30:51 +0300

    Update page.tsx

--- src/app/(public)/pharmacies/page.tsx
+++ src/app/(public)/pharmacies/page.tsx
@@ -1,9 +1,9 @@
 import { Container } from "@/shared/Container";
 
-export default function MedicineStorPage() {
+export default function PharmaciesPage() {
   return (
     <Container className="h-40 flex items-center justify-center">
-      <h1 className="text-5xl text-green-dark">Medicine Stor Page</h1>
+      <h1 className="text-5xl text-green-dark">Pharmacies Page</h1>
     </Container>
   );
 }
