56be8ce2ec043bd48061010150ba611c71223edc
Yuriy <>
2025-07-01 14:39:05 +0300

    Update page.tsx

--- src/app/(public)/pharmacies/page.tsx
+++ src/app/(public)/pharmacies/page.tsx
@@ -2,8 +2,10 @@ import { Container } from "@/shared/Container";
 
 export default function PharmaciesPage() {
   return (
-    <Container className="h-40 flex items-center justify-center">
-      <h1 className="text-5xl text-green-dark">Pharmacies Page</h1>
-    </Container>
+    <>
+      <Container className="h-40 flex items-center justify-center">
+        <h1 className="text-5xl text-green-dark">Pharmacies Page</h1>
+      </Container>
+    </>
   );
 }
