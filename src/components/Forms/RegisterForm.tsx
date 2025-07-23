bb66ad91c5be72219fa1fdcaa48e05704f448022
Yuriy <>
2025-07-23 23:32:14 +0300

    Update RegisterForm.tsx

--- src/components/Forms/RegisterForm.tsx
+++ src/components/Forms/RegisterForm.tsx
@@ -56,136 +56,142 @@ export function RegisterForm() {
       onSubmit={handleSubmit(onSubmit)}
       className="w-full flex flex-col tablet:flex-row tablet:flex-wrap tablet:justify-between"
     >
-      <div className="relative group tablet:max-w-[300px]">
-        <User
-          className={clsx(
-            "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
-            errors.name
-              ? "text-red-dark"
-              : "text-gray-dark group-focus-within:text-green-light"
+      <div className="flex flex-col gap-1 tablet:max-w-[300px] w-full">
+        <div className="relative group">
+          <User
+            className={clsx(
+              "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
+              errors.name
+                ? "text-red-dark"
+                : "text-gray-dark group-focus-within:text-green-light"
+            )}
+          />
+          <input
+            {...register("name")}
+            className={clsx(
+              "w-full pl-10 pr-4 py-2 border rounded-lg focus:ring-2 focus:ring-green-dark focus:border-transparent",
+              errors.name ? "border-red-dark" : "border-gray-dark"
+            )}
+            placeholder="Enter full name"
+          />
+        </div>
+        <div className="h-6 mt-1">
+          {errors.name && (
+            <p className="text-red-dark text-sm">{errors.name.message}</p>
           )}
-        />
-        <input
-          {...register("name")}
-          className={clsx(
-            "w-full pl-10 pr-4 py-2 border-2 rounded-lg focus:ring-2 focus:ring-green-dark focus:border-transparent",
-            errors.name ? "border-red-dark" : "border-green-light"
-          )}
-          placeholder="Enter full name"
-        />
+        </div>
       </div>
-      <div className="h-6 mt-1">
-        {errors.name && (
-          <p className="text-red-dark text-sm">{errors.name.message}</p>
-        )}
+      <div className="flex flex-col gap-1 tablet:max-w-[300px] w-full">
+        <div className="relative group tablet:max-w-[300px]">
+          <Mail
+            className={clsx(
+              "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
+              errors.email
+                ? "text-red-dark"
+                : "text-gray-dark group-focus-within:text-green-light"
+            )}
+          />
+          <input
+            {...register("email")}
+            type="email"
+            className={clsx(
+              "w-full pl-10 pr-4 py-2 border rounded-lg focus:ring-2 focus:ring-green-light focus:border-transparent",
+              errors.email ? "border-red-dark" : "border-gray-dark"
+            )}
+            placeholder="example@email.com"
+          />
+        </div>
+        <div className="h-6 mt-1">
+          {errors.email && (
+            <p className="text-red-dark text-sm">{errors.email.message}</p>
+          )}
+        </div>
       </div>
-
-      <div className="relative group tablet:max-w-[300px]">
-        <Mail
-          className={clsx(
-            "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
-            errors.email
-              ? "text-red-dark"
-              : "text-gray-dark group-focus-within:text-green-light"
+      <div className="flex flex-col gap-1 tablet:max-w-[300px] w-full">
+        <div className="relative group">
+          <Lock
+            className={clsx(
+              "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
+              errors.password
+                ? "text-red-dark"
+                : "text-gray-dark group-focus-within:text-green-light"
+            )}
+          />
+          <input
+            {...register("password")}
+            type={showPassword ? "text" : "password"}
+            className={clsx(
+              "w-full pl-10 pr-10 py-2 border rounded-lg focus:ring-2 focus:ring-green-light focus:border-transparent",
+              errors.password ? "border-red-dark" : "border-gray-dark"
+            )}
+            placeholder="Введіть пароль"
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
+          >
+            {showPassword ? (
+              <EyeOff className="w-5 h-5" />
+            ) : (
+              <Eye className="w-5 h-5" />
+            )}
+          </button>
+        </div>
+        <div className="h-6 mt-1">
+          {errors.password && (
+            <p className="text-red-dark text-sm">{errors.password.message}</p>
           )}
-        />
-        <input
-          {...register("email")}
-          type="email"
-          className={clsx(
-            "w-full pl-10 pr-4 py-2 border rounded-lg focus:ring-2 focus:ring-green-light focus:border-transparent",
-            errors.email ? "border-red-dark" : "border-gray-dark"
-          )}
-          placeholder="example@email.com"
-        />
+        </div>
       </div>
-      <div className="h-6 mt-1">
-        {errors.email && (
-          <p className="text-red-dark text-sm">{errors.email.message}</p>
-        )}
+      <div className="flex flex-col gap-1 tablet:max-w-[300px] w-full">
+        <div className="relative group">
+          <Lock
+            className={clsx(
+              "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
+              errors.confirmPassword
+                ? "text-red-dark"
+                : "text-gray-dark group-focus-within:text-green-light"
+            )}
+          />
+          <input
+            {...register("confirmPassword")}
+            type={showConfirmPassword ? "text" : "password"}
+            className={clsx(
+              "w-full pl-10 pr-10 py-2 border rounded-lg focus:ring-2 focus:ring-green-light focus:border-transparent",
+              errors.confirmPassword ? "border-red-dark" : "border-gray-dark"
+            )}
+            placeholder="Повторіть пароль"
+          />
+          <button
+            type="button"
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-dark hover:text-gray-dark"
+          >
+            {showConfirmPassword ? (
+              <EyeOff className="w-5 h-5" />
+            ) : (
+              <Eye className="w-5 h-5" />
+            )}
+          </button>
+        </div>
+        <div className="h-6 mt-1">
+          {errors.confirmPassword && (
+            <p className="text-red-dark text-sm">
+              {errors.confirmPassword.message}
+            </p>
+          )}
+        </div>
       </div>
-
-      <div className="relative group tablet:max-w-[300px]">
-        <Lock
-          className={clsx(
-            "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
-            errors.password
-              ? "text-red-dark"
-              : "text-gray-dark group-focus-within:text-green-light"
-          )}
-        />
-        <input
-          {...register("password")}
-          type={showPassword ? "text" : "password"}
-          className={clsx(
-            "w-full pl-10 pr-10 py-2 border rounded-lg focus:ring-2 focus:ring-green-light focus:border-transparent",
-            errors.password ? "border-red-dark" : "border-gray-dark"
-          )}
-          placeholder="Введіть пароль"
-        />
+      <div className="w-full flex justify-center mt-2">
         <button
-          type="button"
-          onClick={() => setShowPassword(!showPassword)}
-          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
+          type="submit"
+          disabled={loading || isSubmitting}
+          className="w-full max-w-[300px] bg-green-light text-white-true py-3 px-4 rounded-lg hover:bg-green-dark disabled:opacity-50 disabled:cursor-not-allowed transition-colors font-medium"
         >
-          {showPassword ? (
-            <EyeOff className="w-5 h-5" />
-          ) : (
-            <Eye className="w-5 h-5" />
-          )}
+          {loading || isSubmitting ? "Реєстрація..." : "Зареєструватися"}
         </button>
       </div>
-      <div className="h-6 mt-1">
-        {errors.password && (
-          <p className="text-red-dark text-sm">{errors.password.message}</p>
-        )}
-      </div>
-
-      <div className="relative group tablet:max-w-[300px]">
-        <Lock
-          className={clsx(
-            "absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 transition-colors",
-            errors.confirmPassword
-              ? "text-red-dark"
-              : "text-gray-dark group-focus-within:text-green-light"
-          )}
-        />
-        <input
-          {...register("confirmPassword")}
-          type={showConfirmPassword ? "text" : "password"}
-          className={clsx(
-            "w-full pl-10 pr-10 py-2 border rounded-lg focus:ring-2 focus:ring-green-light focus:border-transparent",
-            errors.confirmPassword ? "border-red-dark" : "border-gray-dark"
-          )}
-          placeholder="Повторіть пароль"
-        />
-        <button
-          type="button"
-          onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-dark hover:text-gray-dark"
-        >
-          {showConfirmPassword ? (
-            <EyeOff className="w-5 h-5" />
-          ) : (
-            <Eye className="w-5 h-5" />
-          )}
-        </button>
-      </div>
-      <div className="h-6 mt-1">
-        {errors.confirmPassword && (
-          <p className="text-red-dark text-sm">
-            {errors.confirmPassword.message}
-          </p>
-        )}
-      </div>
-
-      <button
-        type="submit"
-        disabled={loading || isSubmitting}
-        className="w-full max-w-[300px] bg-green-light text-white-true py-2 px-4 rounded-lg hover:bg-green-dark disabled:opacity-50 disabled:cursor-not-allowed transition-colors font-medium"
-      >
-        {loading || isSubmitting ? "Реєстрація..." : "Зареєструватися"}
-      </button>
     </form>
   );
 }
