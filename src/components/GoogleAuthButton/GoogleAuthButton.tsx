4ed0d91ca6fce01851fcb268a3cea0a0085cdb9f
Yuriy <>
2025-07-24 00:06:45 +0300

    Update GoogleAuthButton.tsx

--- src/components/GoogleAuthButton/GoogleAuthButton.tsx
+++ src/components/GoogleAuthButton/GoogleAuthButton.tsx
@@ -88,15 +88,20 @@ export function GoogleAuthButton({
       onClick={handleGoogleAuth}
       disabled={disabled || isLoading}
       className={`
-        w-full flex justify-center items-center border border-gray-300 rounded-lg shadow-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed transition-colors
+        w-full max-w-[400px] flex justify-center items-center rounded-lg shadow-sm font-medium text-white-true bg-green-light hover:bg-green-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-dark disabled:opacity-50 disabled:cursor-not-allowed transition-colors
         ${sizeClasses[size]}
         ${className}
       `}
+      // className={`
+      //   w-full max-w-[400px] flex justify-center items-center border border-gray-dark rounded-lg shadow-sm font-medium text-black-true bg-green-light hover:bg-green-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed transition-colors
+      //   ${sizeClasses[size]}
+      //   ${className}
+      // `}
     >
       {isLoading ? (
         <div className="flex items-center">
           <svg
-            className={`animate-spin -ml-1 mr-3 text-gray-700 ${spinnerSizes[size]}`}
+            className={`animate-spin -ml-1 mr-3 text-black-true ${spinnerSizes[size]}`}
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
