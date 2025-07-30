57762b0591cf9ba132fc53063ff780e2737eba88
Yuriy <>
2025-07-30 19:51:44 +0300

    Update UserCartAndIcon.tsx

--- src/components/UserCartAndIcon/UserCartAndIcon.tsx
+++ src/components/UserCartAndIcon/UserCartAndIcon.tsx
@@ -23,45 +23,6 @@ export const UserCartAndIcon = () => {
     return name.charAt(0).toUpperCase();
   };
 
-  //   return (
-  //     <div className="flex items-center gap-4 relative">
-  //       <Link
-  //         href="/cart"
-  //         aria-label="Go to shopping cart"
-  //         className="relative flex items-center justify-center w-11 h-11 bg-white-true/20 rounded-full"
-  //       >
-  //         <ShoppingCart className="w-6 h-6 text-white-true" />
-  //         {cartCount >= 0 && (
-  //           <span className="absolute -top-1 -right-2 flex items-center justify-center text-[10px] text-white-true bg-green-dark rounded-full h-5 min-w-5 px-1">
-  //             {cartCount > 99 ? "99+" : cartCount}
-  //           </span>
-  //         )}
-  //       </Link>
-
-  //       <Link
-  //         href="/profile"
-  //         aria-label="Go to profile"
-  //         className="flex items-center justify-center w-11 h-11 bg-white-true/20 rounded-full"
-  //       >
-  //         {userAvatar ? (
-  //           <img
-  //             src={userAvatar}
-  //             alt="User avatar"
-  //             // className="w-full h-full object-cover"
-  //             className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
-  //           />
-  //         ) : getFirstLetter(userName) ? (
-  //           <span className="text-white-true text-lg font-semibold">
-  //             {getFirstLetter(userName)}
-  //           </span>
-  //         ) : (
-  //           <User className="w-6 h-6 text-white-true" />
-  //         )}
-  //       </Link>
-  //     </div>
-  //   );
-  // };
-
   return (
     <div className="flex items-center gap-4 relative">
       <Link
