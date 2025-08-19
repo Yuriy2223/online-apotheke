9f632facb580a5e49b43d0d686ce73ca0a847466
Yuriy <>
2025-08-20 00:38:07 +0300

    Update FeaturesCarousel.tsx

--- src/components/FeaturesCarousel/FeaturesCarousel.tsx
+++ src/components/FeaturesCarousel/FeaturesCarousel.tsx
@@ -26,7 +26,8 @@ export const FeaturesCarousel = () => {
           {features.map((feature, index) => (
             <div
               key={index}
-              className="flex items-center gap-3 whitespace-nowrap flex-shrink-0 bg-green-soft px-8 py-3 rounded-full mx-4"
+              className="flex items-center gap-3 whitespace-nowrap flex-shrink-0 bg-green-soft
+               px-8 py-3 rounded-full mx-4"
             >
               <Zap className="w-5 h-5 text-green-light flex-shrink-0" />
               <span className="text-black-true font-medium text-sm tablet:text-base">
