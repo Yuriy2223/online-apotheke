b3551ca7650fdd8bbe337ad3e916b618a3981e74
Yuriy <>
2025-08-26 00:40:22 +0300

    Update ForgotPasswordForm.tsx

--- src/components/Forms/ForgotPasswordForm.tsx
+++ src/components/Forms/ForgotPasswordForm.tsx
@@ -93,7 +93,7 @@ export function ForgotPasswordForm({
               Sending...
             </>
           ) : (
-            "Надіслати лист"
+            "Send a letter"
           )}
         </button>
       </form>
